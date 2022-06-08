ae75b00468334bded01bb877f941791a5defaf49
accenture.ecolin1 <accenture.ecolin1@bv.com.br>
2022-06-08 11:52:39 -0300

    add first TDD for login router

---
+++ src/presentation/routes/login-router.spec.js
@@ -0,0 +1,35 @@
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('Should return 400 if no e-mail is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any pass'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
+
+describe('Login Router', () => {
+  test('Should return 400 if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
