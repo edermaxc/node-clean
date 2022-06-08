2e585175530980f9ab1e7510d43e2930394aa114
accenture.ecolin1 <accenture.ecolin1@bv.com.br>
2022-06-08 10:14:43 -0300

    initial dev config with husk, lintstaged, standard and jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+     
+  collectCoverage: true,  
+  
+  
+  coverageDirectory: "coverage",
+
+  testEnvironment: "Node",
+
+  
+  coverageProvider: "v8",    
+};
