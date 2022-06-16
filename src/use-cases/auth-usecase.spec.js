450d379d67f03fdc84c11e26001e06a95b40f302
accenture.ecolin1 <accenture.ecolin1@bv.com.br>
2022-06-16 00:12:41 -0300

    ensure authUseCase return an access token if valid credentials are provided

--- src/use-cases/auth-usecase.spec.js
+++ src/use-cases/auth-usecase.spec.js
@@ -106,4 +106,17 @@ describe('Auth UseCase', () => {
     await sut.auth('valid_email@email.com', 'valid_password')
     expect(tokenGeneratorSpy.userId).toBe(loadUserByEmailRepositorySpy.user.id)
   })
+
+  test('Should calls TokenGenerator with correct UserId', async () => {
+    const { sut, loadUserByEmailRepositorySpy, tokenGeneratorSpy } = makeSut()
+    await sut.auth('valid_email@email.com', 'valid_password')
+    expect(tokenGeneratorSpy.userId).toBe(loadUserByEmailRepositorySpy.user.id)
+  })
+
+  test('Should returns an accessToken if correct credentials are provided', async () => {
+    const { sut, tokenGeneratorSpy } = makeSut()
+    const accessToken = await sut.auth('valid_email@email.com', 'valid_password')
+    expect(accessToken).toBe(tokenGeneratorSpy.accessToken)
+    expect(accessToken).toBeTruthy()
+  })
 })
