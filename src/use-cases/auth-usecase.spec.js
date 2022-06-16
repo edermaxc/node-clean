500a64f3ea0ee722bdb28c73fdfff6a6c3adf858
accenture.ecolin1 <accenture.ecolin1@bv.com.br>
2022-06-15 23:16:08 -0300

    ensure AuthUseCase is pasing correct email to LoadUserByEmailRepository

--- src/use-cases/auth-usecase.spec.js
+++ src/use-cases/auth-usecase.spec.js
@@ -1,6 +1,10 @@
 const { MissingParamError } = require('../utils/error')
 
 class AuthUseCase {
+  constructor (loadUserByEmailRepository) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+  }
+
   async auth (email, pass) {
     if (!email) {
       throw new MissingParamError('email')
@@ -8,12 +12,23 @@ class AuthUseCase {
     if (!pass) {
       throw new MissingParamError('password')
     }
+    await this.loadUserByEmailRepository.load(email)
   }
 }
 
 const makeSut = () => {
-  const sut = new AuthUseCase()
-  return { sut }
+  const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepository()
+  const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
+  return { sut, loadUserByEmailRepositorySpy }
+}
+
+const makeLoadUserByEmailRepository = () => {
+  class LoadUserByEmailRepositorySpy {
+    async load (email) {
+      this.email = email
+    }
+  }
+  return new LoadUserByEmailRepositorySpy()
 }
 
 describe('Auth UseCase', () => {
@@ -22,13 +37,16 @@ describe('Auth UseCase', () => {
     const promise = sut.auth()
     expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
-})
 
-describe('Auth UseCase', () => {
   test('Should throws if no password is provided', async () => {
     const { sut } = makeSut()
     const promise = sut.auth('email')
     expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
-})
 
+  test('Should throws if no password is provided', async () => {
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
+    await sut.auth('any_email@email.com', 'amy_password')
+    expect(loadUserByEmailRepositorySpy.email).toBe('any_email@email.com')
+  })
+})
