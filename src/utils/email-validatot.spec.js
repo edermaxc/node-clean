2f7b0097c31ba989ad1d35f9ae1331d3bea2fe79
accenture.ecolin1 <accenture.ecolin1@bv.com.br>
2022-06-15 22:22:14 -0300

    add tests for email validator

--- src/utils/email-validatot.spec.js
+++ src/utils/email-validatot.spec.js
@@ -6,17 +6,27 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
-  test('Shuold return true if validator returns true', () => {
-    const sut = new EmailValidator()
+  test('Should return true if validator returns true', () => {
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@email.com')
     expect(isEmailValid).toBe(true)
   })
 
-  test('Shuold return false if validator returns false', () => {
-    const sut = new EmailValidator()
+  test('Should return false if validator returns false', () => {
+    const sut = makeSut()
     validator.isEmailValid = false
     const isEmailValid = sut.isValid('invalid_email@email.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+    sut.isValid('any_email@email.com')
+    expect(validator.email).toBe('any_email@email.com')
   })
 })
