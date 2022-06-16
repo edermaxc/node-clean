743b50863fdd7e96a55ea4ee47300c427290632d
accenture.ecolin1 <accenture.ecolin1@bv.com.br>
2022-06-15 22:51:47 -0300

    ensure AuthUseCase return MissinParamError if no password is provided

--- src/use-cases/auth-usecase.spec.js
+++ src/use-cases/auth-usecase.spec.js
@@ -5,13 +5,30 @@ class AuthUseCase {
     if (!email) {
       throw new MissingParamError('email')
     }
+    if (!pass) {
+      throw new MissingParamError('password')
+    }
   }
+}
+
+const makeSut = () => {
+  const sut = new AuthUseCase()
+  return { sut }
 }
 
 describe('Auth UseCase', () => {
   test('Should throws if no e-mail is provided', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const promise = sut.auth()
     expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 })
+
+describe('Auth UseCase', () => {
+  test('Should throws if no password is provided', async () => {
+    const { sut } = makeSut()
+    const promise = sut.auth('email')
+    expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+})
+
