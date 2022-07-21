708b83d02a8ef1b357d251c9f1cd73e2945baf39
accenture.ecolin1 <accenture.ecolin1@bv.com.br>
2022-07-20 22:05:00 -0300

    ensure Encrypter returns false is bcrypt returns false

--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -1,12 +1,21 @@
+const bcrypt = require('bcrypt')
+
 class Encrypter {
-  async compare (password, hashedPassword) {
-    return true
+  async compare (value, hash) {
+    return await bcrypt.compare(value, hash)
   }
 }
 describe('Encrypter', () => {
   test('Shuld return true if bcrypt returns true', async () => {
     const sut = new Encrypter()
-    const isValid = await sut.compare('any_password', 'hashed_password')
+    const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(true)
+  })
+
+  test('Shuld return false if bcrypt returns false', async () => {
+    const sut = new Encrypter()
+    bcrypt.isValid = false
+    const isValid = await sut.compare('any_value', 'hashed_value')
+    expect(isValid).toBe(false)
   })
 })
