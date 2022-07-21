4250359e1f58fc2090438d62549ad99798bceb32
accenture.ecolin1 <accenture.ecolin1@bv.com.br>
2022-07-20 22:08:14 -0300

    ensure encrypter call bcrypt with correct values

--- __mocks__/bcrypt.js
+++ __mocks__/bcrypt.js
@@ -1,7 +1,11 @@
 module.exports = {
   isValid: true,
+  value: '',
+  hash: '',
 
   async compare (value, hashedValue) {
+    this.value = value
+    this.hash = hashedValue
     return this.isValid
   }
 }
