bdab41b5270a5579fcb6e0ed44097b53a626d729
accenture.ecolin1 <accenture.ecolin1@bv.com.br>
2022-07-20 22:21:10 -0300

    ensure encrypter throws if no params are provided

--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -1,28 +1,35 @@
 const bcrypt = require('bcrypt')
 const Encrypter = require('./encrypter')
+const MissingParamError = require('../error/missing-param-error')
 
 const makeSut = () => {
   return new Encrypter()
 }
 
 describe('Encrypter', () => {
-  test('Shuld return true if bcrypt returns true', async () => {
+  test('Should return true if bcrypt returns true', async () => {
     const sut = makeSut()
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(true)
   })
 
-  test('Shuld return false if bcrypt returns false', async () => {
+  test('Should return false if bcrypt returns false', async () => {
     const sut = makeSut()
     bcrypt.isValid = false
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(false)
   })
 
-  test('Shuld call bcrypt with correct values', async () => {
+  test('Should call bcrypt with correct values', async () => {
     const sut = makeSut()
     await sut.compare('any_value', 'hashed_value')
     expect(bcrypt.value).toBe('any_value')
     expect(bcrypt.hash).toBe('hashed_value')
+  })
+
+  test('Should throws if no params are provided', async () => {
+    const sut = makeSut()
+    expect(sut.compare()).rejects.toThrow(new MissingParamError('value'))
+    expect(sut.compare('any_value')).rejects.toThrow(new MissingParamError('hash'))
   })
 })
